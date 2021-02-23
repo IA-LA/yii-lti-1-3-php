c78d36d73e41a7773337285c1d448aafa3e7ddc3
FJ <fsanchez@lsi.uned.es>
2021-02-23 14:33:44 +0100

    Yii: fornmularios Query y Register. Register.

--- models/RegisterForm.php
+++ models/RegisterForm.php
@@ -25,8 +25,11 @@ class RegisterForm extends Model
         return [
             // id, url, subject and body are required
             [['id', 'url', 'subject', 'body'], 'required'],
-            // id has to be a valid ID alphanumerical 16 character address
-            ['id', 'id'],
+            // id has to be a valid ID alphanumerical 24 character address
+            ['id', 'length',
+                'is' => 24,
+                'tooLong'=>'Has to be a valid ObjectId alphanumerical 24 character address like this 5fc3860a81740b0ef098a965',
+                'tooShort'=>'Has to be a valid ObjectId alphanumerical 24 character address like this 5fc3860a81740b0ef098a965'],
             // url has to be a valid URL address
             ['url', 'url'],
             // verifyCode needs to be entered correctly
