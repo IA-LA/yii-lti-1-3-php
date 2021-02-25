cfcd5580b2946818be503d85523d83ed95bf7782
FJ <fsanchez@lsi.uned.es>
2021-02-25 14:23:55 +0100

    Yii: fornmularios Query y Register. Query.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -283,7 +283,7 @@ class SiteController extends Controller
                 ->setFormat(Client::FORMAT_JSON)
                 //->setMethod('POST')
                 ->setMethod('GET')
-                ->setUrl($url . '/read/' . Yii::$app->request->post()->data['QueryForm']['id'])//$_POST['QueryForm']['id'])
+                ->setUrl($url . '/read/' . Yii::$app->request->bodyParams)//$_POST['QueryForm']['id'])
                 ->setData(['name' => 'John Doe', 'email' => 'johndoe@domain.com'])
                 ->setOptions([
                     //'proxy' => 'tcp://proxy.example.com:5100', // use a Proxy
