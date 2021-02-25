856ce4a0843681a173a91fd06d0fa3a9854b3aad
FJ <fsanchez@lsi.uned.es>
2021-02-25 10:51:08 +0100

    Yii: fornmularios Query y Register. Register.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -196,7 +196,7 @@ class SiteController extends Controller
             //return $this->renderContent('<div><p/><p/><p/><p class="alert alert-success"> Registro finalizado: ' . ArrayHelper::isAssociative($request) . ArrayHelper::getValue($request, 'RegisterForm') . print_r($request) . print_r($response) . '</p></div>');
             //             return $this->renderContent('<div><p/><p/><p/><p class="alert alert-success"> Registro finalizado: ' . ArrayHelper::isAssociative($request) . '<br/>REQUEST:<br/> . print_r($request) . <br/>RESPONSE:<br/> . print_r($response)' . print_r($request) . '</p></div><br/>');
             $content='<div><p/><p/><p/><p class="alert alert-success"> Registro realizado: ' . ArrayHelper::isAssociative($request) . '</p></div><br/>';
-            //$content.='<div><p/><p/><p/><p class="alert alert-success"> REQUEST : ' . print_r($request) . '</p></div><br/>';
+            $content.='<div><p/><p/><p/><p class="alert alert-success"> REQUEST : ' . print_r($request) . '</p></div><br/>';
             //$content.='<div><p/><p/><p/><p class="alert alert-success">RESPONSE: ' . print_r($request) . '</p></div><br/>';
             return $this->renderContent($content);
 
