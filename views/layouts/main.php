7eaa1a661a15ef738579aa968194d35d5489fb5b
FJ <fsanchez@lsi.uned.es>
2021-02-26 14:28:38 +0100

    Yii: nombres del servidor y compañía.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -107,7 +107,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= Yii::$app->params['yiiname'] ?> <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
