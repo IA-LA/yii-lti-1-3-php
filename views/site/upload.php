737b601c8f0288084805a8d2a423e6c0346b68bd
FJ <fsanchez@lsi.uned.es>
2021-02-28 13:10:14 +0100

    Yii: fornmulario Upload. Upload captcha.

--- views/site/upload.php
+++ views/site/upload.php
@@ -41,10 +41,6 @@ $this->params['breadcrumbs'][] = $this->title;
                             <!-- UPLOAD Bad Request (#400) Unable to verify your data submission.   -->
                             <input type="hidden" name="_csrf" value="<?=Yii::$app->request->getCsrfToken()?>" />
 
-                            <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                                'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                            ]) ?>
-
                         </div>
 
                     </div>
