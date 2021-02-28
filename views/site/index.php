fc41fe9f8cbb40ab5b34ac9ed2628ebc60965365
FJ <fsanchez@lsi.uned.es>
2021-02-28 12:18:07 +0100

    Yii: Index y compañía.

--- views/site/index.php
+++ views/site/index.php
@@ -19,7 +19,7 @@ $this->title = Yii::$app->params['yiiapp'];
         }
         else{
         ?>
-            <p class="lead">Bienvenido, <?= ' "' . Yii::$app->user->identity->username . '" ' ?>accede al panel de Actividades (Tools).</p>
+            <p class="lead">Bienvenido, <?= ' "<i>' . Yii::$app->user->identity->username . '</i>" ' ?>accede al panel de Actividades (Tools).</p>
 
             <form action="/index.php?r=site%2Flogout" method="post">
                 <input type="hidden" name="_csrf" value="<?=Yii::$app->request->getCsrfToken()?>">
