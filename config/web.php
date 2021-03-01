e88a4168aac624c2e9e5ae373329c74bc09df6b2
FJ <fsanchez@lsi.uned.es>
2021-03-01 13:46:34 +0100

    Yii: Index y compañía. Config

--- config/web.php
+++ config/web.php
@@ -70,13 +70,14 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+if (YII_ENV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
@@ -84,7 +85,7 @@ if (YII_ENV_DEV) {
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*'],
+        'allowedIPs' => ['*'],
     ];
 }
 
