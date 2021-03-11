6bd3bd907511ea44351204a7e73b793f581f2138
FJ <fsanchez@lsi.uned.es>
2021-03-11 10:24:00 +0100

    Yii: listado de registros y avisos. ListView.

---
+++ controllers/ListController.php
@@ -0,0 +1,46 @@
+<?php
+// YOUR_APP/controllers/ListController.php
+
+/* change namespace in your app */
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\data\ArrayDataProvider;
+
+class ListController extends Controller
+{
+    public function actionIndex()
+    {
+        $provider = new ArrayDataProvider([
+            'allModels' => $this->getFakedModels(),
+            'pagination' => [
+                'pageSize' => 5
+            ],
+            'sort' => [
+                'attributes' => ['id'],
+            ],
+        ]);
+
+        return $this->render('index', ['listDataProvider' => $provider]);
+    }
+
+    // function to generate faked models, don't care about this.
+    private function getFakedModels()
+    {
+        $fakedModels = [];
+
+        for ($i = 1; $i < 18; $i++) {
+            $fakedItem = [
+                'id' => $i,
+                'title' => 'Title ' . $i,
+                'image' => 'http://placehold.it/300x200'
+            ];
+
+            $fakedModels[] = $fakedItem;
+        }
+
+        return $fakedModels;
+    }
+}
+?>
