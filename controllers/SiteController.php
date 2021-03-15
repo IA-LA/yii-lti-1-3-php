d87abfb7fba1b3cd3845d08872d24baa9692d4a7
FJ <fsanchez@lsi.uned.es>
2021-03-15 16:56:10 +0100

    Yii: fornmularios Lists. ListView.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -616,7 +616,7 @@ exit(0);
                 foreach ($response->data as $Item => $value){
                     //print(json_decode($Item['data'], true));
                     if($Item === 'data')
-                        echo "{$Item} => {$value}";
+                        echo "{$Item} => " . json_decode($value, true);
                     /*$responseItem = [
                         'list' => 'iss',
                         'id' => $Item['data']['launch_parameters']['iss'],
