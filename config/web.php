e55e311c8e7bb47239ba745c84b8cb32aa2f1b68
FJ <fsanchez@lsi.uned.es>
2021-03-16 12:39:59 +0100

    Yii: fornmularios Upload, List, Query y Register. Users DB.

--- config/web.php
+++ config/web.php
@@ -20,7 +20,7 @@ $config = [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => 'yii\db\Connection',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
