3dcd33961ca75299b16b06a455640a0430448e4b
FJ <fsanchez@lsi.uned.es>
2021-03-18 13:45:13 +0100

    Yii: Index y compañía. Publicacion Zip.

--- views/site/index.php
+++ views/site/index.php
@@ -85,6 +85,7 @@ $this->title = Yii::$app->params['yiiapp'];
             echo "Returned with status $retval and output:\n";
             print_r($output);
 
+            // Descomprime .zip
             // outputs the username that owns the running php/httpd process
             // (on a system with the "unzip" executable in the path)
             $output=null;
@@ -93,7 +94,10 @@ $this->title = Yii::$app->params['yiiapp'];
             //exec(escapeshellcmd('unzip uploads/CANVAS_QTI_IMPORT_UNIT_TEST.zip -d uploads/publicacion/nombreTrabajoXXX00000000/'), $output, $retval);
             exec(escapeshellcmd('unzip uploads/cindetececontentv1_5a5db903d3bd0d7623bc10c0.zip -d uploads/publicacion/' . $namedir), $output, $retval);
             echo "Returned with status $retval and output:\n";
-            print_r($output);
+            echo "<pre>". print_r($output) . "</pre>";
+
+            // Registra ID=$namedir y URL='uploads/publicacion/$namedir/'
+            ////////////////////////////////
 
             ?>
         </div>
