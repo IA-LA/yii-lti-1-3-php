d532e9ab356fc21dce00089c4900b8e9ec73e637
FJ <fsanchez@lsi.uned.es>
2021-03-19 22:27:26 +0100

    Yii: fornmularios Upload, Borrado, List, Query y Register. Upload mkdir unzip y register con aciertos y errores.

--- views/site/upload.php
+++ views/site/upload.php
@@ -65,9 +65,10 @@ $this->params['breadcrumbs'][] = $this->title;
                         <p/>
                         <p/>
                         <p/>
-                        <p class="alert alert-success">Fichero ZIP ´<i><?= $file ?></i>´ descomprimido correctamente. URL de la Actividad ´<b><a href="uploads/publicacion/<?= Html::encode($namedir); ?>" target="_blank"><?= $namedir ?></a></b>´ publicada correctamente</p>
+                        <p class="alert alert-success">Fichero ZIP ´<i><?= $file ?></i>´ fue descomprimido correctamente.<br/>URL de la Actividad ´<b><a href="uploads/publicacion/<?= Html::encode($namedir); ?>" target="_blank"><?= $namedir ?></a></b>´ publicada correctamente</p>
                 <?php
-                        echo "Returned with status $retval and output:\n";
+                        //echo "Returned with status $retval and output:\n";
+                        echo "<b>Archivos descomprimidos" . strlen($output) . ". Status " . ($retval === 0 ? 'correcto' : 'error') . ". y resultado de la descompresión:\n</b>";
                         echo "<p><pre>";
                         print_r($output);
                         echo "</pre></p>";
