675b72cc766f3c9e510e552c62d67f579f433f44
FJ <fsanchez@lsi.uned.es>
2021-03-21 13:56:12 +0100

    Yii: Desarrollo. Mkdir, Unzip, URL y Git. Git.

--- views/site/development.php
+++ views/site/development.php
@@ -45,7 +45,7 @@ $this->title = Yii::$app->params['yiiapp'];
 
             // Git
             $output = shell_exec(escapeshellcmd('git --version'));
-            echo "<pre>$output</pre>";
+            echo "<pre>1. $output</pre>";
             // Carpeta de Git
             // outputs the username that owns the running php/httpd process
             // (on a system with the "mkdir" executable in the path)
@@ -53,7 +53,7 @@ $this->title = Yii::$app->params['yiiapp'];
             $retval=null;
             exec(escapeshellcmd('mkdir uploads/git'), $output, $retval);
             echo "Returned with status $retval and output:\n";
-            echo "<p><pre>";
+            echo "<p><pre>2.";
             print_r($output);
             echo "</pre></p>";
 
@@ -67,17 +67,18 @@ $this->title = Yii::$app->params['yiiapp'];
             $namedir= Yii::$app->user->identity->id . date('YmdHisu') . 'a';
             exec(escapeshellcmd('mkdir uploads/git/' . $namedir . '.git'), $output, $retval);
             echo "Returned with status $retval and output:\n";
-            echo "<p><pre>";
+            echo "<p><pre>3.";
             print_r($output);
             echo "</pre></p>";
 
             // Proyecto Git
             // Crear Git vacío distribuíble (--bare)
             $output = shell_exec(escapeshellcmd('git --bare -C uploads/git/ init ' . $namedir . '.git'));
-            echo "<pre>$output</pre>";
+            echo "<pre>4.$output</pre>";
             // Clonar Git distribuido
+            //$output = shell_exec(escapeshellcmd('git -C uploads/publicacion/ clone uploads/git/' . $namedir . '.git ' . $namedir));
             $output = shell_exec(escapeshellcmd('git clone uploads/git/' . $namedir . '.git uploads/publicacion/' . $namedir));
-            echo "<pre>$output</pre>";
+            echo "<pre>5.$output</pre>";
 
             // Unzip Actividad .zip
             // outputs the username that owns the running php/httpd process
@@ -90,16 +91,79 @@ $this->title = Yii::$app->params['yiiapp'];
             //print_r($output);
             //echo "</pre></p>";
             $output = shell_exec(escapeshellcmd('echo "Hola Mundo Linux" >> uploads/publicacion/' . $namedir . '/HolaMundo.txt'));
-            echo "<pre>$output</pre>";
+            echo "<pre>6.$output</pre>";
 
             // Add, Commit y Push publicacion
             $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ add .'));
-            echo "<pre>$output</pre>";
+            echo "<pre>7.$output</pre>";
             $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ commit'));
-            echo "<pre>$output</pre>";
+            echo "<pre>8.$output</pre>";
             $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ push'));
-            echo "<pre>$output</pre>";
+            echo "<pre>9.$output</pre>";
 
+            // INICIO
+            // outputs the username that owns the running php/httpd process
+            // (on a system with the "whoami" executable in the path)
+            $output=null;
+            $retval=null;
+            exec('whoami', $output, $retval);
+            echo "Returned with status $retval and output:\n";
+            echo "<p><pre>";
+            print_r($output);
+            echo "</pre></p>";
+
+            //$output = shell_exec(escapeshellcmd('ls -lart'));
+            //echo "<pre>$output</pre>";
+
+            //$output = shell_exec('ls -lart /');
+            //echo "<pre>$output</pre>";
+
+            // Carpeta de cargas
+            $output = shell_exec(escapeshellcmd('ls -lart uploads'));
+            echo "<pre>$output</pre>";
+            //$output = shell_exec(escapeshellcmd('touch uploads/index.html'));
+            //echo "<pre>$output</pre>";
+
+            // Carpeta de publicación
+            $output = shell_exec(escapeshellcmd('ls -lart /var/www/html/ | mkdir uploads/publicacion'));
+            echo "<pre>$output</pre>";
+            $output = shell_exec(escapeshellcmd('ls -lart uploads/publicacion'));
+            echo "<pre>$output</pre>";
+            //$output = shell_exec(escapeshellcmd('touch uploads/publicacion/index.html'));
+            //echo "<pre>$output</pre>";
+
+            //mkdir('/var/www/html/lti/publicacion/nombreTrabajoXXX00000001', 0777, true);
+
+            // Carpeta de Actividad cargada y publicada
+            // Convenio de nombre actividades (24 hex) y carpeta = id user + fecha y hora + 'a'
+            /////////////////////////////////
+            // outputs the username that owns the running php/httpd process
+            // (on a system with the "mkdir" executable in the path)
+            $output=null;
+            $retval=null;
+            //$namedir= substr('nombreTrabajo',0, (strlen('nombreTrabajo') - strlen(Yii::$app->user->identity->username) >=0 ? strlen('nombreTrabajo') - strlen(Yii::$app->user->identity->username) : 0)) . Yii::$app->user->identity->username . date('YmdHisu') . '00000003';
+            $namedir= Yii::$app->user->identity->id . date('YmdHisu') . 'a';
+            exec(escapeshellcmd('mkdir uploads/publicacion/' . $namedir), $output, $retval);
+            echo "Returned with status $retval and output:\n";
+            echo "<p><pre>";
+            print_r($output);
+            echo "</pre></p>";
+
+            // Descomprime .zip
+            // outputs the username that owns the running php/httpd process
+            // (on a system with the "unzip" executable in the path)
+            $output=null;
+            $retval=null;
+            //'unzip uploads/Plantilla\ ePub\ 1_5c4ad1844ffce90a5d17f666.zip -d uploads/publicacion/nombreTrabajoXXX00000000/'
+            //exec(escapeshellcmd('unzip uploads/CANVAS_QTI_IMPORT_UNIT_TEST.zip -d uploads/publicacion/nombreTrabajoXXX00000000/'), $output, $retval);
+            exec(escapeshellcmd('unzip uploads/cindetececontentv1_5a5db903d3bd0d7623bc10c0.zip -d uploads/publicacion/' . $namedir), $output, $retval);
+            echo "Returned with status $retval and output:\n";
+            echo "<p><pre>";
+            print_r($output);
+            echo "</pre></p>";
+
+            // Registra ID=$namedir y URL='uploads/publicacion/$namedir/'
+            ////////////////////////////////
 
             ?>
 
