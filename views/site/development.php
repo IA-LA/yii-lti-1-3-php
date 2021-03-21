f56165541102ef702626aa35f4baa8da4839c910
FJ <fsanchez@lsi.uned.es>
2021-03-21 19:30:34 +0100

    Yii: Desarrollo. Mkdir, Unzip, URL y Git. Git.

--- views/site/development.php
+++ views/site/development.php
@@ -84,7 +84,7 @@ $this->title = Yii::$app->params['yiiapp'];
 
             // Proyecto Git
             // Crear Git vacío distribuíble (--bare)
-            $output = shell_exec(escapeshellcmd('git --bare -C uploads/git/' . $namedir . '.git init'));
+            $output = shell_exec(escapeshellcmd('git --bare -C uploads/git/ init ' . $namedir . '.git'));
             echo "<pre>4.$output</pre>";
             // Clonar Git distribuido
             //$output = shell_exec(escapeshellcmd('git -C uploads/publicacion/ clone uploads/git/' . $namedir . '.git ' . $namedir));
