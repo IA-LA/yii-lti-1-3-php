ce4a8939d8416435f06fc2916245d19d8d8119ab
FJ <fsanchez@lsi.uned.es>
2021-03-22 10:18:31 +0100

    Yii: fornmularios Upload, Borrado, List, Query y Register. List texto.

--- views/site/lists.php
+++ views/site/lists.php
@@ -34,7 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php else: ?>
 
         <p>
-            Consulta la información de una Actividad ya registrada por su Id o su Url.
+            Lista la información de una/s Actividad/es ya registrada/s por su Id o su Url.
             Thank you.
         </p>
 
