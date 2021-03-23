07bcc9d25debd07c48426c91053b32c58677042c
FJ <fsanchez@lsi.uned.es>
2021-03-23 14:33:38 +0100

    Yii: fornmularios Upload, Borrado, List, Query y Register. Upload Git.

--- views/site/upload.php
+++ views/site/upload.php
@@ -236,13 +236,13 @@ $this->params['breadcrumbs'][] = $this->title;
                         <p/>
                         <p/>
                         <p/>
-                        <p class="alert alert-success">
-                            <ul>
+                        <div class="alert alert-success">
+                            <ol>
                                 <li>Git URL de la Actividad ´<b><i><a href="uploads/git/<?= Html::encode($namedir); ?>.git" target="_blank"><?= $namedir ?></a></i></b>´ generado correctamente.<br/></li>
                                 <li>Fichero de la Actividad ´<b><i><?= $file ?></i></b>´ descomprimido correctamente.<br/></li>
                                 <li>Web URL de la Actividad ´<b><i><a href="uploads/publicacion/<?= Html::encode($namedir); ?>" target="_blank"><?= $namedir ?></a></i></b>´ publicada correctamente</li>
-                            </ul>
-                        </p>
+                            </ol>
+                        </div>
                 <?php
 
                         // TODO Registra ID=$namedir y URL='uploads/publicacion/$namedir/'
