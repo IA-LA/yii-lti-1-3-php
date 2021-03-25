3c57f7367419db381f53f2d2019321e6f0b54a68
FJ <fsanchez@lsi.uned.es>
2021-03-25 14:00:30 +0100

    Yii: Desarrollo. Mkdir, Unzip, URL y Git. Git. Unzip .zip.

--- views/site/development.php
+++ views/site/development.php
@@ -119,10 +119,23 @@ $this->title = Yii::$app->params['yiiapp'];
             $retval=null;
             //exec(escapeshellcmd('unzip uploads/' . $file . ' -d uploads/publicacion/' . $namedir), $output, $retval);
             //exec(escapeshellcmd('unzip uploads/cindetechtmlv1_5a5db903d3bd0d7623bc10c0.zip -d uploads/publicacion/' . $namedir), $output, $retval);
+            exec(escapeshellcmd('umask 0022'), $output, $retval);
+            echo "6.Returned with status $retval and output:\n";
+            echo "<p><pre> 6.a. Umask<br/>";
+            print_r($output);
+            echo "8.a.PassThru " . passthru('umask 0022 2>&1') . "<br/>";
+            echo "</pre></p>";
+            // outputs the username that owns the running php/httpd process
+            // (on a system with the "unzip" executable in the path)
+            $output=null;
+            $retval=null;
+            //exec(escapeshellcmd('unzip uploads/' . $file . ' -d uploads/publicacion/' . $namedir), $output, $retval);
+            //exec(escapeshellcmd('unzip uploads/cindetechtmlv1_5a5db903d3bd0d7623bc10c0.zip -d uploads/publicacion/' . $namedir), $output, $retval);
             exec(escapeshellcmd('unzip uploads/600ff7059d5e4d1174e7c425_5e46670337ebc61534f37c4a_5e46673e37ebc61534f37c4d.zip -d uploads/publicacion/' . $namedir), $output, $retval);
             echo "6.Returned with status $retval and output:\n";
             echo "<p><pre> 6.a. Unzip<br/>";
             print_r($output);
+            echo "8.a.PassThru " . passthru('unzip uploads/600ff7059d5e4d1174e7c425_5e46670337ebc61534f37c4a_5e46673e37ebc61534f37c4d.zip -d uploads/publicacion/' . $namedir . ' 2>&1') . "<br/>";
             echo "</pre></p>";
             //$output = shell_exec(escapeshellcmd('echo "Hola Mundo Linux" >> uploads/publicacion/' . $namedir . '/HolaMundo.txt'));
             //echo "<pre>6.$output</pre>";
