ffc680ba24b15627643dc77e9b6df71ba2f1485d
FJ <fsanchez@lsi.uned.es>
2021-03-29 12:51:57 +0200

    Yii: fornmularios Upload, Borrado, List, Query y Register. List index view format.

--- views/lists/_list_item.php
+++ views/lists/_list_item.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 ?>
 
 <article class="list-item col-sm-12" data-key="<?= $model['id'] ?>">
-    <h3><?= Html::encode($model['id']); ?></h3>
-    <figure>
+    <h4><?= Html::encode($model['id']); ?></h4>
+    <h5>
         <?= $model['title'] ?> <?= $model['link'] ?> <img src="<?= $model['image'] ?>" alt="<?= $model['id'] ?>" width="30">
-    </figure>
+    </h5>
 </article>
