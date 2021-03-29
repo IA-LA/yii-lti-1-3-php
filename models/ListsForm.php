98043f72bb48355dc5c8ccf5f98ae5a02b40add7
FJ <fsanchez@lsi.uned.es>
2021-03-29 10:50:51 +0200

    Yii: fornmularios Upload, Borrado, List, Query y Register. List wildcard `*`.

--- models/ListsForm.php
+++ models/ListsForm.php
@@ -37,7 +37,7 @@ class ListsForm extends Model
             // id has to be a valid ID hexadecimal 24 character address
             //['id', 'match', 'pattern'=>"/^[a-f,0-9]{24}$/u", 'message'=>'Has to be a valid Mongo ObjectId hexadecimal 24 character address like this: `5fc3860a81740b0ef098a965`'],
             //['id', 'match', 'pattern'=>"/^([a-f,0-9]{24})|[*]$/u", 'message'=>'Has to be a valid Mongo ObjectId hexadecimal 24 character address like this: `5fc3860a81740b0ef098a965` or the wildcard * to list all registers.'],
-            ['id', 'match', 'pattern'=>"/^([a-f0-9]{24})|([a-f0-9]{0:23})[*]|[*]([a-f0-9]{0:22})|[*]$/u", 'message'=>'Has to be a valid Mongo ObjectId hexadecimal 24 character address like this: `5fc3860a81740b0ef098a965` or with the wildcard * to list all registers.'],
+            ['id', 'match', 'pattern'=>"/^([a-f0-9]{24})|([a-f0-9]{0:23})[*]|[*]([a-f0-9]{0:23})|[*]([a-f0-9]{0:22})|[*]$/u", 'message'=>'Has to be a valid Mongo ObjectId hexadecimal 24 character address like this: `5fc3860a81740b0ef098a965` or with the wildcard * to list all registers in that ways: *, HH..HH*, *HH..HH, *HH..HH*.'],
             // url has to be a valid URL address
             ['url', 'url', 'message'=>'Has to be a valid URL address like `http://contenido.uned.es/`'],
             // verifyCode needs to be entered correctly
