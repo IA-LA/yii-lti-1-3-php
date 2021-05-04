2e5b9368eeffda6144d4f69976b9cf83f061c615
FJ <fsanchez@lsi.uned.es>
2021-05-04 14:45:10 +0200

    Yii: Controlador CRUD. Fornmularios Borrado, List, Query y Register. Controlador y Menú Main Rutas CRUD genéricas.

--- views/crud/register.php
+++ views/crud/register.php
@@ -35,7 +35,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <p>
             Registra una Actividad con su Identificador y la Url donde está alojada.
-            <?php echo Yii::$app->params['r'] . Yii::$app->params['coleccion'] ?>
+            <?php echo $r . $coleccion ?>
             Thank you.
         </p>
 
