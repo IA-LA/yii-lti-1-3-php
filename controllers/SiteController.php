2746a33cf2f5d8a6649da8bc30b7eb06e8147ee6
FJ <fsanchez@lsi.uned.es>
2021-05-04 13:23:52 +0200

    Yii: fornmularios Upload, Borrado, List, Query y Register. Borrado. Rutas CRUD genéricas.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -680,10 +680,11 @@ exit(0);
                 // DELETE Register (https://stackoverflow.com/questions/19905118/how-to-call-rest-api-from-view-in-yii)
                 $client = new Client();
 
+                // Buscar por _ID
                 if (Yii::$app->request->post('DeleteForm')['id'] !== '') {
                     // http://10.201.54.31:49151/servicios/lti/lti13/delete/5e0df19c0c2e74489066b43g
                     $ruta = '/delete/coleccion/Lti/id_actividad/' . Yii::$app->request->post('DeleteForm')['id'];
-                } else {
+                } else { // Buscar por URL
                     // http://10.201.54.31:49151/servicios/lti/lti13/delete/5e0df19c0c2e74489066b43g
                     $ruta = '/delete/coleccion/Lti/id_actividad/' . str_replace('+', '%20', urlencode(Yii::$app->request->post('deleteForm')['id']));
                 }
