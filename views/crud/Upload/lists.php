742a1c71f6b604520629d67067e9726bc018cd90
FJ <fsanchez@lsi.uned.es>
2021-05-05 13:04:30 +0200

    Yii: Controlador CRUD. Modelos y Vistas. Fornmularios Borrado, List, Query y Register. Registro. Rutas CRUD genéricas.

---
+++ views/crud/Upload/lists.php
@@ -0,0 +1,65 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\crud\Upload\ListsForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
+
+$this->title = 'Lists Upload';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-lists">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php if (Yii::$app->session->hasFlash('listsFormSubmitted')): ?>
+
+        <div class="alert alert-success">
+            Thank you for contacting us. We will respond to you as soon as possible.
+        </div>
+
+        <p>
+            Note that if you turn on the Yii debugger, you should be able
+            to view the mail message on the mail panel of the debugger.
+            <?php if (Yii::$app->mailer->useFileTransport): ?>
+                Because the application is in development mode, the email is not sent but saved as
+                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
+                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
+                application component to be false to enable email sending.
+            <?php endif; ?>
+        </p>
+
+    <?php else: ?>
+
+        <p>
+            Lista la información de una/s Actividad/es ya registrada/s por su Id o su Url.<br/>
+            Admite el comodín * en estas formas (*, HH..HH*, *HH..HH *HH..HH*).<br/>
+            Thank you.
+        </p>
+
+        <div class="row">
+            <div class="col-lg-5">
+
+                <?php $form = ActiveForm::begin(['id' => 'lists-form']); ?>
+
+                    <?= $form->field($model, 'id')->textInput(['autofocus' => true]) ?>
+
+                    <?= $form->field($model, 'url') ?>
+
+                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                    ]) ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'lists-button']) ?>
+                    </div>
+
+                <?php ActiveForm::end(); ?>
+
+            </div>
+        </div>
+
+    <?php endif; ?>
+</div>
