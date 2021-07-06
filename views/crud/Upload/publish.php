36ce7eacd3c0d5b51d08ccafe143d8c356833d64
FJ <fsanchez@lsi.uned.es>
2021-07-06 18:43:12 +0200

    Yii: Upload y UploadRegister. UploadRegister. Reparado error `Pull` master.

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -63,7 +63,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ pull'), $output, $retval);
                 exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ commit -m "Commit Publish Git" .'), $output, $retval);
                 // REPOSITORIO SIN CAMBIOS O ACTUALIZADO CORRECTAMENTE
-                if(($output === "Already up to date.") || ($output === "Merge made by the 'recursive' strategy.")) {
+                if((strpos($output, "Already up to date.")) || (strpos($output, "Merge made by the 'recursive' strategy."))) {
                     echo "10.Returned with status $retval and output:\n";
                     echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ pull origin master<br/>";
                     echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1') . "<br/>";
