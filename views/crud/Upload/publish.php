06de6523f38abf4bc76cfd239b0c17a9e28aacb5
FJ <fsanchez@lsi.uned.es>
2021-07-06 17:43:32 +0200

    Yii: Upload y UploadRegister. UploadRegister. Prueba error PULL.

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -64,7 +64,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ commit -m "Commit Publish Git" .'), $output, $retval);
                 echo "10.Returned with status $retval and output:\n";
                 echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ pull origin master<br/>";
-                echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull 2>&1') . "<br/>";
+                echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1') . "<br/>";
                 print_r($output);
                 echo "</pre></p>";
                 $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'));
