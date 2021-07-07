1a4b1066ef5ceabc30dc9944f7afc5d3ec30585d
FJ <fsanchez@lsi.uned.es>
2021-07-07 10:09:41 +0200

    Yii: Publish Git Menu. Reparado error `Pull` master.

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -65,7 +65,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 echo "10.Returned with status $retval and output:\n";
                 echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ pull origin master<br/>";
                 print_r($output);
-                echo "10.PassThru" . $output = passthru(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $retvar)) . "<br/>";
+                echo "10.PassThru" . $output = passthru(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'), $retvar) . "<br/>";
                 print($retvar);
                 echo "</pre></p>";
                 $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'));
