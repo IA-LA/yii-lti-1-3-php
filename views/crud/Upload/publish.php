53e658afddceceb25810d46e67b9138e46ed21eb
FJ <fsanchez@lsi.uned.es>
2021-07-07 10:41:02 +0200

    Yii: Publish Git Menu. Reparado error `Pull` master.

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -60,23 +60,28 @@ $this->params['breadcrumbs'][] = $this->title;
                 // Pull Repositirio
                 $output=null;
                 $retval=null;
+                $retva=null;
+                $retv=null;
+                $ret=null;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ pull'), $output, $retval);
                 exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ commit -m "Commit Publish Git" .'), $output, $retval);
                 echo "10.Returned with status $retval and output:\n";
                 echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ pull origin master<br/>";
                 print_r($output);
-                echo "10.PassThru" . $output = passthru(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'), $retvar) . "<br/>";
-                print($retvar);
+                echo "10.PassThru" . $output = passthru(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'), $retva) . "<br/>";
+                print($retva);
                 echo "</pre></p>";
-                $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'));
+                $output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'), $retv);
                 echo "<pre>10.b. $output</pre>";
-                $output = system(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'));
+                print($retv);
+                $output = system(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'), $ret);
                 echo "<pre>10.c. $output</pre>";
+                print($ret);
 
                 // Pull Git Publicacion sin errores
                 //REPOSITORIO ACTUALIZADO CORRECTAMENTE
                 // !strpos($output, "Merge made by the 'recursive' strategy."))
-                if(($retval === 0) || ($retvar === 0)) {
+                if(($retval === 0) || ($retva === 0)) {
             ?>
                     <div class="alert alert-success">
                         <ol>
