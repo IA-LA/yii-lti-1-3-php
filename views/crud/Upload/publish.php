97d9a13fc85ddb7a8e0c6f1b7048a9e12794619e
FJ <fsanchez@lsi.uned.es>
2021-07-07 17:56:01 +0200

    Yii: Publish Git Menu. Reparado error `Pull` master.

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -83,7 +83,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 // Pull Git Publicacion sin errores
                 //REPOSITORIO ACTUALIZADO CORRECTAMENTE
-                if(($retval === 0) || (strpos(array_reduce($output[count($output)-1]), "Merge made by the 'recursive' strategy.")) ) {
+                if(($retval === 0) || (strpos($output[count($output)-1], "Merge made by the 'recursive' strategy.")) ) {
             ?>
                     <div class="alert alert-success">
                         <ol>
@@ -108,7 +108,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 }
                 else {
                     // REPOSITORIO SIN CAMBIOS
-                    if(strpos(array_reduce($output[count($output)-1]), 'Already up to date.')) {
+                    if(strpos($output[count($output)-1], 'Already up to date.')) {
             ?>
                     <div class="alert alert-success">
                         <ol>
