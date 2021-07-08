e448d5e1fddaa40f411b80f41ecc0e2b334f825a
FJ <fsanchez@lsi.uned.es>
2021-07-08 20:05:47 +0200

    Yii: Publish Menu. Publish & Register.

--- views/site/about.php
+++ views/site/about.php
@@ -21,32 +21,66 @@ $this->params['breadcrumbs'][] = $this->title;
         <h2>TODO</h2>
         <ol>
             <li><code>DONE</code> Subir a `uploads/` y publicar .zip eContent URL `publicacion/IdDateTimea/`</li>
-            <li><code>DONE</code> Proyecto Git la Actividad (eContrent) como `git/IdDateTimea.git`</li>
-            <li><code>TODO</code> Almcenar las subidas de .zip, publicación y proyecto en la colección de la BBDD.</li>
-            <li><code>TODO</code> Registrar automáticamente ID + URL de la Actividad (eContent) en el lServidor LTI</li>
-            <li><code>TODO</code> Proveer la Actividad (eContent) en entornos LMS (EdX y Moodle)</li>
+            <li><code>DONE</code> Crear Proyecto Git la Actividad (eContrent) como `git/IdDateTimea.git`</li>
+            <li><code>DONE</code> Almcenar los Uploads de .zip, publicación y proyecto Git en la colección `Uploads`de la BBDD.</li>
+            <li><code>DONE</code> Almcenar las Actividades publicadas en la colección `Ltis`de la BBDD.</li>
+            <li><code>TODO</code> Registrar automáticamente ID + URL de la Actividad (eContent) publicada en el lServidor LTI</li>
+            <li><code>TODO</code> Registrar automáticamente ZIP + URL y Proyecto Git de la Upload en el lServidor LTI</li>
+            <li><code>TODO</code> Proveer la Actividad (eContent) en entornos LMS (EdX, Moodle, etc)</li>
         </ol>
         <div class="row">
             <div class="col-lg-4">
-                <h2>Git Menu</h2>
+                <h2>Main Menu</h2>
                 <hr/>
-                <h3>Upload</h3>
+                <h3>Upload Activity</h3>
 
-                <p>Permite subir contenidos complejos, en un único archivo .zip sin espacios en blanco, eñes o tildes en
+                <p>Permite subir y registrar contenidos complejos, en un único archivo .zip sin espacios en blanco, eñes o tildes en
                     el nombre, el cual se utilizará como plantilla para publicarlo en una URL, registrarlo en el Servidor
                     LTI y como base de un proyecto Git que permita trabajar en él de forma distribuida.</p>
 
                 <p><a class="btn btn-default" href="index.php?r=site%2Fupload">Upload &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=site%2Fuploadregister">Upload & Register &raquo;</a></p>
 
-                <h3>Upload Menu</h3>
-                <ol>
-                    <li><code>TODO</code> Subir a `uploads/` y publicar .zip eContent URL `publicacion/IdDateTimea/`</li>
-                    <li><code>TODO</code> Proyecto Git la Actividad (eContrent) como `git/IdDateTimea.git`</li>
-                    <li><code>TODO</code> Almcenar las subidas de .zip, publicación y proyecto en la colección de la BBDD.</li>
-                    <li><code>TODO</code> Registrar automáticamente ID + URL de la Actividad (eContent) en el lServidor LTI</li>
-                    <li><code>TODO</code> Proveer la Actividad (eContent) en entornos LMS (EdX y Moodle)</li>
-                </ol>
+                <hr/>
+                <h3>Publish Project</h3>
 
+                <p>Permite publicar y actualizar contenidos complejos, con un id sin espacios en blanco, eñes o tildes en
+                    el nombre, el cual se utilizará como plantilla para publicarlo en una URL, registrarlo en el Servidor
+                    LTI y como base de un proyecto Git que permita trabajar en él de forma distribuida.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=site%2Fpublish">Publish &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=site%2Fpublishregister">Publish & Register &raquo;</a></p>
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Git Menu</h2>
+                <hr/>
+                <h3>Delete</h3>
+
+                <p>Formulario de borrado de Proyectos (Git) dadas de alta en el servior.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=crud%2Fdelete">Borrado &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Lists</h3>
+
+                <p>Formulario de listados de Proyectos (Git) dadas de alta en el servior.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=crud%2Flists">Listados &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Query</h3>
+
+                <p>Formulario de consulta de Proyectos (Git) dadas de alta en el servior.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=crud%2Fquery">Consulta &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Register</h3>
+
+                <p>Formulario que permite dar de alta un Proyecto (Git) para su gestión.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=crud%2Fregister">Registro &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>LTI Menu</h2>
@@ -74,7 +108,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-lg-4">
                 <h3>Register</h3>
 
-                <p>Formulario que permite dar de alta una Actividad (Tool) para su publicación.</p>
+                <p>Formulario que permite dar de alta una Actividad (Tool) para su difusión.</p>
 
                 <p><a class="btn btn-default" href="index.php?r=site%2Fregister">Registro &raquo;</a></p>
             </div>
