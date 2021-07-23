529aba93e9b29554c23c75eae840cd6c3282b8ba
FJ <fsanchez@lsi.uned.es>
2021-07-23 10:09:01 +0200

    Yii: Añade Registro Git automático. Upload & Register.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -309,7 +309,7 @@ class SiteController extends Controller
 
                     // REGISTER Publicación & LTI
                     // --------------------------
-                    // LLAMADA POST Register
+                    // LLAMADA POST Register 1
                     // Información servidor
                     //  https://www.php.net/manual/es/function.header.php
                     ///////////////////////
@@ -327,7 +327,7 @@ class SiteController extends Controller
                     // Llamadas REST
                     //  https://stackoverflow.com/questions/2445276/how-to-post-data-in-php-using-file-get-contents
                     //  https://www.php.net/manual/en/context.http.php
-                    // Obtiene la configuración de las actividades con una llamada de lectura `GET`
+                    // Envía la configuración de las actividades con una llamada de escritura `POST`
                     // al servidor de SERVICIOS
                     ///////////////////
                     /// LOCAL puerto :9000
@@ -402,15 +402,15 @@ class SiteController extends Controller
                     //return $this->renderContent('<div><p/><p/><p/><p class="alert alert-success"> Registro finalizado: ' . ArrayHelper::isAssociative($request) . ArrayHelper::getValue($request, 'RegisterForm') . print_r($request) . print_r($response) . '</p></div>');
                     //return $this->renderContent('<div><p/><p/><p/><p class="alert alert-success"> Registro finalizado: ' . ArrayHelper::isAssociative($request) . '<br/>REQUEST:<br/> . print_r($request) . <br/>RESPONSE:<br/> . print_r($response)' . print_r($request) . '</p></div><br/>');
                     // yii\httpclient\Response Object ( [client] => yii\httpclient\Client Object ( [baseUrl] => [formatters] => Array ( [urlencoded] => yii\httpclient\UrlEncodedFormatter Object ( [encodingType] => 1 [charset] => ) ) [parsers] => Array ( ) [requestConfig] => Array ( ) [responseConfig] => Array ( ) [contentLoggingMaxSize] => 2000 [_transport:yii\httpclient\Client:private] => yii\httpclient\StreamTransport Object ( [_events:yii\base\Component:private] => Array ( ) [_eventWildcards:yii\base\Component:private] => Array ( ) [_behaviors:yii\base\Component:private] => ) [_events:yii\base\Component:private] => Array ( ) [_eventWildcards:yii\base\Component:private] => Array ( ) [_behaviors:yii\base\Component:private] => Array ( ) ) [_headers:yii\httpclient\Message:private] => Array ( [0] => HTTP/1.1 200 OK [1] => X-Powered-By: Express [2] => Content-Type: application/json; charset=utf-8 [3] => Content-Length: 1180 [4] => ETag: W/"49c-2mm6tdE08PBK3Du9hRlhHVqbw2Y" [5] => Date: Thu, 25 Feb 2021 09:53:53 GMT [6] => Connection: close ) [_cookies:yii\httpclient\Message:private] => [_content:yii\httpclient\Message:private] => {"result":"ok","data":{"user":{"email":"nadie@uned.es","nombre":"Nadie","rol":"Administrador"},"launch_parameters":{"iss":"5fc3860a81740b0ef098a983","login_hint":"123456","target_link_uri":"http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43g/index_default.html","lti_message_hint":"123456"},"credentials":{"deployment":["8c49a5fa-f955-405e-865f-3d7e959e809f"],"client_id":"d42df408-70f5-4b60-8274-6c98d3b9468d","auth_login_url":"http://10.201.54.31:9002/platform/login.php","auth_token_url":"http://10.201.54.31:9002/platform/token.php","key_set_url":"http://10.201.54.31:9002/platform/jwks.php","private_key_file":"/private.key","auth_server":"http://10.201.54.31:9002/platform/login.php","kid":"58f36e10-c1c1-4df0-af8b-85c857d1634f"},"_id":"5fc3860a81740b0ef098a983","id_actividad":"5fc3860a81740b0ef098a983","url_actividad":"http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43g/index_default.html","launch_url":"http://10.201.54.31:9002/login.php?iss=5fc3860a81740b0ef098a983&login_hint=123456&target_link_uri=http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43g/index_default.html<i_message_hint=123456","zf":"2020-12-17T09:01:03.889Z"}} [_data:yii\httpclient\Message:private] => [_format:yii\httpclient\Message:private] => [_events:yii\base\Component:private] => Array ( ) [_eventWildcards:yii\base\Component:private] => Array ( ) [_behaviors:yii\base\Component:private] => )
-                    if ($response->isOk && $response->data['result'] === 'ok' && $response->data['data']['result'] != 'Existe') {
+                    if ($response->isOk && $response->data['result'] === 'ok' && $response->data['data']['result'] === 'Existe') {
                         $content = '<div><p/><p/><p/><p class="alert alert-success"> Registro: ' . $response->data['result'] . '</p>';
                         $content .= '<div class="jumbotron">
-                        <h1>Registro</h1>
-                        <p class="lead">Copia las credenciales de acceso a la actividad.</p>' .
+                            <h1>Registro</h1>
+                            <p class="lead">Copia las credenciales de acceso a la actividad.</p>' .
                             'LAUNCH URL: <code>' . Html::encode($response->data['data']['launch_url']) . '</code><br/>' .
                             '<p/><p/><p/>' .
                             '<p><a class="btn btn-lg btn-success" href="index.php?r=site%2Fregister">Atrás</a></p>
-                    </div>';
+                        </div>';
                         $content .= '</div>';
 
                     } else {
@@ -421,18 +421,138 @@ class SiteController extends Controller
                         //$content .= '<div><p/><p/><p/><p class="alert alert-success">RESPONSE: ' . print_r($response) . '</p></div><br/>';
                         //$content.= '<button class="btn btn-info" onclick="history.go(-1);return false;">Atrás</button>';
                         $content .= '<div class="jumbotron">
-                        <h1>Error</h1>
-                        <p class="lead">Las credenciales de Registro son erróneas.</p>' .
+                            <h1>Error</h1>
+                            <p class="lead">Las credenciales de Registro son erróneas.</p>' .
                             'ID:  <code>' . $namefile . '</code><br/>' .
                             'URL: <code>' . $serverPub . '/' . $namefile . '</code><br/>' .
                             '<p/><p/><p/>' .
                             '<p><a class="btn btn-lg btn-warning" href="index.php?r=site%2Fregister">Atrás</a></p>
-                    </div>';
+                        </div>';
                         $content .= '</div>';
                     }
 
                     // REGISTER Upload & Git
                     // ---------------------
+                    // LLAMADA POST Register 1
+                    // Información servidor
+                    //  https://www.php.net/manual/es/function.header.php
+
+                    // POST
+                    // POST Register (https://stackoverflow.com/questions/19905118/how-to-call-rest-api-from-view-in-yii)
+                    $client = new Client();
+
+                    if ($namefile !== '') {
+                        // http://10.201.54.31:49151/servicios/lti/lti13/create/register/coleccion/:coleccion
+                        $ruta = '/create/register/coleccion/Upload';
+                    } else {
+                        // http://10.201.54.31:49151/servicios/lti/lti13/create/coleccion/coleccion/url_actividad/https://www.uned.es
+                        $ruta = '/create/coleccion/Upload/url_actividad/' . str_replace('+', '%20', urlencode(Yii::$app->request->post('RegisterForm')['url']));
+                    }
+
+                    // Exception POST LTI
+                    try {
+                        // Dirección de alojamiento
+                        // del servidor de Git
+                        //////////////////////
+                        /// LOCAL puerto :9000
+                        /// GLOBAL puerto:8000 o `.uned.es`
+                        ///
+                        if ((! strpos($_SERVER['HTTP_HOST'], '.uned.es')) && ($_SERVER['REMOTE_PORT'] !== '80') && ($_SERVER['REMOTE_PORT'] !== '8000')) {
+                            $serverPublicacion = Yii::$app->params['serverPublicacion_local'];
+                            $carpetaPublicacion = Yii::$app->params['carpetaPublicacion_local'];
+                            $serverGit = Yii::$app->params['serverGit_local'];
+                            $carpetaGit = Yii::$app->params['carpetaGit_local'];
+                            $serverLti = Yii::$app->params['serverLti_local'];
+                        }
+                        else {
+                            $serverPublicacion = Yii::$app->params['serverPublicacion_global'];
+                            $carpetaPublicacion = Yii::$app->params['carpetaPublicacion_global'];
+                            $serverGit = Yii::$app->params['serverGit_global'];
+                            $carpetaGit = Yii::$app->params['carpetaGit_global'];
+                            $serverLti = Yii::$app->params['serverLti_global'];
+                        }
+
+                        $response = $client->createRequest()
+                            ->setFormat(Client::FORMAT_JSON)
+                            ->setMethod('POST')
+                            //->setMethod('GET')
+                            ->setUrl($url . $ruta)
+                            //$_POST['RegisterForm']['id']) Parámetros del registro
+                            ->setData([
+                                    'id_actividad' => $namefile,
+                                    'url_actividad' => $serverPublicacion . '/' . $namefile,
+                                    'fichero' => $uploadregister['file'],
+                                    'carpeta' => $namefile,
+                                    'publicacion_url' => $serverPublicacion . '/' . $namefile, //['publicacion']
+                                    'git_url' => $serverGit . '/' . $namefile . '.git' //['git']
+                                ]
+                            )
+                            ->setOptions([
+                                //'proxy' => 'tcp://proxy.example.com:5100', // use a Proxy
+                                'timeout' => 5, // set timeout to 5 seconds for the case server is not responding
+                            ])
+                            ->send();
+                    }
+                    catch (Exception $e1) {
+                        // Exception POST LTI2
+                        try {
+                        }
+                        catch (Exception $e2) {
+                        }
+                    }
+                    //foreach ($request as $key => $value){
+                    //    echo "{$key} => {$value} ";
+                    //}
+                    // Array ( [_csrf] => _jj1OVZYhyxeDkVwF82Lt-ANf6mPzL_xKv5nCNCp7H-4daVqPx3eXm1LGjpNq8Tut3RMnMCex7dQlStFhZC9LQ== [RegisterForm] => Array ( [id] => 012345678901234567890123 [url] => http://127.0.0.1:8000/index.php?r=site%2Fregister [subject] => a [body] => aa [verifyCode] => zuvagi ) [register-button] => )
+                    //return $this->renderContent('<div><p/><p/><p/><p class="alert alert-success"> Registro finalizado: ' . ArrayHelper::isAssociative($request) . ArrayHelper::getValue($request, 'RegisterForm') . print_r($request) . print_r($response) . '</p></div>');
+                    //return $this->renderContent('<div><p/><p/><p/><p class="alert alert-success"> Registro finalizado: ' . ArrayHelper::isAssociative($request) . '<br/>REQUEST:<br/> . print_r($request) . <br/>RESPONSE:<br/> . print_r($response)' . print_r($request) . '</p></div><br/>');
+                    // yii\httpclient\Response Object ( [client] => yii\httpclient\Client Object ( [baseUrl] => [formatters] => Array ( [urlencoded] => yii\httpclient\UrlEncodedFormatter Object ( [encodingType] => 1 [charset] => ) ) [parsers] => Array ( ) [requestConfig] => Array ( ) [responseConfig] => Array ( ) [contentLoggingMaxSize] => 2000 [_transport:yii\httpclient\Client:private] => yii\httpclient\StreamTransport Object ( [_events:yii\base\Component:private] => Array ( ) [_eventWildcards:yii\base\Component:private] => Array ( ) [_behaviors:yii\base\Component:private] => ) [_events:yii\base\Component:private] => Array ( ) [_eventWildcards:yii\base\Component:private] => Array ( ) [_behaviors:yii\base\Component:private] => Array ( ) ) [_headers:yii\httpclient\Message:private] => Array ( [0] => HTTP/1.1 200 OK [1] => X-Powered-By: Express [2] => Content-Type: application/json; charset=utf-8 [3] => Content-Length: 1180 [4] => ETag: W/"49c-2mm6tdE08PBK3Du9hRlhHVqbw2Y" [5] => Date: Thu, 25 Feb 2021 09:53:53 GMT [6] => Connection: close ) [_cookies:yii\httpclient\Message:private] => [_content:yii\httpclient\Message:private] => {"result":"ok","data":{"user":{"email":"nadie@uned.es","nombre":"Nadie","rol":"Administrador"},"launch_parameters":{"iss":"5fc3860a81740b0ef098a983","login_hint":"123456","target_link_uri":"http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43g/index_default.html","lti_message_hint":"123456"},"credentials":{"deployment":["8c49a5fa-f955-405e-865f-3d7e959e809f"],"client_id":"d42df408-70f5-4b60-8274-6c98d3b9468d","auth_login_url":"http://10.201.54.31:9002/platform/login.php","auth_token_url":"http://10.201.54.31:9002/platform/token.php","key_set_url":"http://10.201.54.31:9002/platform/jwks.php","private_key_file":"/private.key","auth_server":"http://10.201.54.31:9002/platform/login.php","kid":"58f36e10-c1c1-4df0-af8b-85c857d1634f"},"_id":"5fc3860a81740b0ef098a983","id_actividad":"5fc3860a81740b0ef098a983","url_actividad":"http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43g/index_default.html","launch_url":"http://10.201.54.31:9002/login.php?iss=5fc3860a81740b0ef098a983&login_hint=123456&target_link_uri=http://10.201.54.31:9002/Plantilla Azul_5e0df19c0c2e74489066b43g/index_default.html<i_message_hint=123456","zf":"2020-12-17T09:01:03.889Z"}} [_data:yii\httpclient\Message:private] => [_format:yii\httpclient\Message:private] => [_events:yii\base\Component:private] => Array ( ) [_eventWildcards:yii\base\Component:private] => Array ( ) [_behaviors:yii\base\Component:private] => )
+                    if ($response->isOk && $response->data['result'] === 'ok' && $response->data['data']['result'] === 'Existe') {
+                        $content = '<div><p/><p/><p/><p class="alert alert-success"> Registro: ' . $response->data['result'] . '</p>';
+                        $content .= '<div class="jumbotron">
+                            <h1>Registro</h1>
+                            <p class="lead">de Upload realizado correctamente.</p>' .
+                            'ID: <code>' .
+                            Html::encode($response->data['data']['register']['id_actividad']) .
+                            '</code><br/>' .
+                            'URL: <code>' .
+                            Html::encode($response->data['data']['register']['url_actividad']) .
+                            '</code><br/>' .
+                            'FILE: <code>' .
+                            Html::encode($response->data['data']['register']['upload']['fichero']) .
+                            '</code><br/>' .
+                            'FOLDER: <code>' .
+                            'PUBLISH: <code>' .
+                            Html::encode($response->data['data']['register']['upload']['carpeta']) .
+                            '</code><br/>' .
+                            'PUBLICACION: <code>' .
+                            Html::encode($response->data['data']['register']['upload']['publicacion_url']) .
+                            '</code><br/>' .
+                            'GIT: <code>' .
+                            Html::encode($response->data['data']['register']['upload']['git_url']) .
+                            '</code><br/>' .
+                            '<p/><p/><p/>' .
+                            '<p><a class="btn btn-lg btn-success" href="index.php?r=crud%2Fregister">Atrás</a></p>
+                        </div>';
+                        $content .= '</div>';
+
+                    } else {
+                        $content = '<div><p/><p/><p/>';
+                        $content .= '<p class="alert error-summary"> Registro: ' . Yii::$app->request->post('RegisterForm...', 'error') . '</p>';
+                        //$content = '<div><p/><p/><p/><p class="alert alert-success"> Registro: ' . ArrayHelper::isAssociative($request) . '</p></div><br/>';
+                        //$content.='<div><p/><p/><p/><p class="alert alert-success"> REQUEST : ' . print_r($request) . '</p></div><br/>';
+                        //$content .= '<div><p/><p/><p/><p class="alert alert-success">RESPONSE: ' . print_r($response) . '</p></div><br/>';
+                        //$content.= '<button class="btn btn-info" onclick="history.go(-1);return false;">Atrás</button>';
+                        $content .= '<div class="jumbotron">
+                            <h1>Error</h1>
+                            <p class="lead">Las credenciales de Registro son erróneas.</p>' .
+                            'ID:  <code>' . $namefile . '</code><br/>' .
+                            'URL: <code>' . $serverPublicacion . '/' . $namefile . '</code><br/>' .
+                            '<p/><p/><p/>' .
+                            '<p><a class="btn btn-lg btn-warning" href="index.php?r=crud%2Fregister">Atrás</a></p>
+                        </div>';
+                        $content .= '</div>';
+                    }
 
                     // Return file is uploaded successfully
                     ///////////////////////////////////////
