b169ea221e496cb3c14ec41b08434dcd983497e8
FJ <fsanchez@lsi.uned.es>
2021-09-02 14:22:52 +0200

    Yii: Configura los parámetros del Pull. Publish (& Register).

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -103,22 +103,22 @@ $this->params['breadcrumbs'][] = $this->title;
                 //echo "10.Returned with status $retval and output:\n";
                 //echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ commit -m 'Commit Publish Git' .<br/>";
                 //print_r($output);
-                exec('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $output, $retval);
+                exec('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $output, $retva);
                 //echo "10.Returned with status $retva and output:\n";
                 //echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ pull origin master 2>&1<br/>";
                 //print(implode(" ", $output));
-                echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $retval) . "<br/>";
+                echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $retv) . "<br/>";
                 //print($retva);
                 //echo "</pre></p>";
                 shell_exec(escapeshellcmd('$(git -C uploads/publicacion/' . $namedir . '/ pull origin master)'));
                 //echo "<pre>10.b. $output</pre>";
-                system(escapeshellcmd('$(git -C uploads/publicacion/' . $namedir . '/ pull origin master)'), $retval);
+                system(escapeshellcmd('$(git -C uploads/publicacion/' . $namedir . '/ pull origin master)'), $ret);
                 //echo "<pre>10.c. $output</pre>";
                 //print($ret);
 
                 // Pull Git Publicacion sin errores
                 //REPOSITORIO ACTUALIZADO CORRECTAMENTE
-                if(($retval === 0) || (strpos(implode(" ", $output), "Merge made by the 'recursive' strategy.")) ) {
+                if(($retval === 0) || (strpos(implode(" ", $output), "Merge made by the 'recursive' strategy")) ) {
             ?>
                     <div class="alert alert-success">
                         <ol>
