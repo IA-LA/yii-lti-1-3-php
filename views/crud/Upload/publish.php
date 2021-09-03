e1a773449d9e58fc8c8c98b78483ee3bbc8e07c5
FJ <fsanchez@lsi.uned.es>
2021-09-03 09:30:18 +0200

    Yii: Configura los parámetros del Pull. Publish (& Register).

--- views/crud/Upload/publish.php
+++ views/crud/Upload/publish.php
@@ -107,7 +107,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 //echo "10.Returned with status $retva and output:\n";
                 //echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ pull origin master 2>&1<br/>";
                 //print(implode(" ", $output));
-                //echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $retv) . "<br/>";
+                echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ pull origin master 2>&1', $retv) . "<br/>";
                 //print($retva);
                 //echo "</pre></p>";
                 shell_exec(escapeshellcmd('$(git -C uploads/publicacion/' . $namedir . '/ pull origin master)'));
