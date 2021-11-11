62c8aeda7304969218fa50bc2529b8a866102b65
FJ <fsanchez@lsi.uned.es>
2021-11-11 11:41:20 +0100

    Yii: About. Actualiza tareas TODO Y DONE.

--- views/site/about.php
+++ views/site/about.php
@@ -24,8 +24,8 @@ $this->params['breadcrumbs'][] = $this->title;
             <li><code>DONE</code> Crear Proyecto Git la Actividad (eContrent) como `git/IdDateTimea.git`</li>
             <li><code>DONE</code> Almcenar los Uploads de .zip, publicación y proyecto Git en la colección `Uploads`de la BBDD.</li>
             <li><code>DONE</code> Almcenar las Actividades publicadas en la colección `Ltis`de la BBDD.</li>
-            <li><code>DONE</code> Registrar automáticamente ID + URL de la Actividad (eContent) publicada en el lServidor LTI</li>
-            <li><code>DONE</code> Registrar automáticamente ZIP + URL y Proyecto Git de la Upload en el lServidor LTI</li>
+            <li><code>DONE</code> Registrar automáticamente ID + URL de la Actividad (eContent) publicada en el Servidor LTI</li>
+            <li><code>DONE</code> Registrar automáticamente ZIP + URL y Proyecto Git de la Upload en el Servidor LTI</li>
             <li><code>DONE</code> Proveer la Actividad (eContent) en entornos LMS (EdX, Moodle, etc) HTTP</li>
             <li><code>TODO</code> Proveer la Actividad (eContent) en entornos LMS (EdX, Moodle, etc) HTTPS</li>
             <li><code>DONE</code> Proveer el Recurso (LtiResourceLinkRequest) OIDC en entornos <a href="https://lti-ri.imsglobal.org/platforms/2630">IMS Global</a> (simulación)</li>
