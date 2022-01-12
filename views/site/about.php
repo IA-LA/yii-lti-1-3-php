bd38678e451079e30baee67eea205b46e007d60b
FJ <fsanchez@lsi.uned.es>
2022-01-12 19:31:20 +0100

    Yii: About. Actualiza tareas.

--- views/site/about.php
+++ views/site/about.php
@@ -34,8 +34,8 @@ $this->params['breadcrumbs'][] = $this->title;
             <li><code>TODO</code> Paso mensajes LTI Advantage (DeepLinkingRequest DL) en plataformas <a href="http://ailanto-dev.intecca.uned.es:8080" target="_blank">EdX</a>, etc reales</li>
             <li><code>TODO</code> Paso mensajes LTI Advantage (AGS) en plataformas <a href="http://ailanto-dev.intecca.uned.es:8080" target="_blank">EdX</a>, etc reales</li>
             <li><code>TODO</code> Paso mensajes LTI Advantage (NPRS) en plataformas <a href="http://ailanto-dev.intecca.uned.es:8080" target="_blank">EdX</a>, etc reales</li>
-            <li>Update (Git, Lti y Platform)</li>
             <li>Platform Menu</li>
+            <li>Update (Git, Lti y Platform)</li>
         </ol>
         <div class="row">
             <div class="col-lg-4">
