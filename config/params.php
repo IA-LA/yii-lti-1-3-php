e5c73ea39a2c35909eef07cffb8d6da974973c4a
FJ <fsanchez@lsi.uned.es>
2022-01-26 07:34:51 +0100

    Yii: Params. IP Servidor local.

--- config/params.php
+++ config/params.php
@@ -7,7 +7,7 @@ return [
     'serverLti_global' => 'http://ailanto-dev.intecca.uned.es:9002',// 'https://ailanto-dev.intecca.uned.es/lti/lti13', // 'http://10.201.54.31:9002/platform',
     'serverLti_local' => 'http://192.168.0.31:9002',
     'serverServiciosLti_global' => 'https://ailanto-dev.intecca.uned.es/servicios/lti/lti13', // 'http://10.201.54.31:49151/servicios/lti/lti13',
-    'serverServiciosLti_local' => 'http://127.7.0.1:49151/servicios/lti/lti13',
+    'serverServiciosLti_local' => 'http://127.0.0.1:49151/servicios/lti/lti13',
     'serverGit_global' => 'https://ailanto-dev.intecca.uned.es/git', //'http://ailanto-dev.intecca.uned.es/uploads/git', // 'http://10.201.54.31:8000/uploads/git',
     'serverGit_local' => 'http://192.168.0.31:8000/uploads/git',
     'serverPublicacion_global' => 'https://ailanto-dev.intecca.uned.es/lti/publicacion', //'http://ailanto-dev.intecca.uned.es/uploads/publicacion', // 'http://10.201.54.31:8000/uploads/publicacion',
