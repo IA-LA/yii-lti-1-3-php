0019b698be11842ec2809aa5f8d6f243a27925a5
FJ <fsanchez@lsi.uned.es>
2022-01-26 20:19:13 +0100

    Yii: Params. IP Servidor local.

--- config/params.php
+++ config/params.php
@@ -1,22 +1,22 @@
 <?php
 
 return [
+    'yiiapp' => 'LTI Server Client',
+    'yiiname' => 'UNED',
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'serverLti_global' => 'http://ailanto-dev.intecca.uned.es:9002',// 'https://ailanto-dev.intecca.uned.es/lti/lti13', // 'http://10.201.54.31:9002/platform',
-    'serverLti_local' => 'http://192.168.0.31:9002',
+    'serverLti_local' => 'http://192.168.42.10:9002', // 'http://192.168.0.31:9002',
     'serverServiciosLti_global' => 'https://ailanto-dev.intecca.uned.es/servicios/lti/lti13', // 'http://10.201.54.31:49151/servicios/lti/lti13',
-    'serverServiciosLti_local' => 'http://192.168.0.31:49151/servicios/lti/lti13', //'http://127.0.0.1:49151/servicios/lti/lti13' no funciona
+    'serverServiciosLti_local' => 'http://192.168.42.10:49151/servicios/lti/lti13', // 'http://192.168.0.31:49151/servicios/lti/lti13', //'http://127.0.0.1:49151/servicios/lti/lti13' no funciona
     'serverGit_global' => 'https://ailanto-dev.intecca.uned.es/git', //'http://ailanto-dev.intecca.uned.es/uploads/git', // 'http://10.201.54.31:8000/uploads/git',
-    'serverGit_local' => 'http://192.168.0.31:8000/uploads/git',
+    'serverGit_local' => 'http://192.168.42.10:8000/uploads/git', // 'http://192.168.0.31:8000/uploads/git',
     'serverPublicacion_global' => 'https://ailanto-dev.intecca.uned.es/lti/publicacion', //'http://ailanto-dev.intecca.uned.es/uploads/publicacion', // 'http://10.201.54.31:8000/uploads/publicacion',
-    'serverPublicacion_local' => 'http://192.168.0.31:8000/uploads/publicacion',
+    'serverPublicacion_local' => 'http://192.168.42.10:8000/uploads/publicacion', // 'http://192.168.0.31:8000/uploads/publicacion',
     'carpetaGit_global' => '/root/LTI/yii-lti-1-3-php/web/uploads/git', //'/var/www/html/webdav/lti/git',
     'carpetaGit_local' => '/home/francisco/LTI/yii-lti-1-3-php/web/uploads/git',
     'carpetaPublicacion_global' => '/root/LTI/yii-lti-1-3-php/web/uploads/publicacion', //'/var/www/html/webdav/lti/publicacion',
     'carpetaPublicacion_local' => '/home/francisco/LTI/yii-lti-1-3-php/web/uploads/publicacion',
-    'yiiapp' => 'LTI Server',
-    'yiiname' => 'UNED',
 
 ];
