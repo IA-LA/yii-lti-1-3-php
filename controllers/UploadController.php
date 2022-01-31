9c5d4b4deaab668d639693a91a34c56415fb0ffd
FJ <fsanchez@lsi.uned.es>
2022-01-31 06:53:07 +0100

    Yii: Lists. Actualiza Vista LTI, Platfofm, Upload y Site.

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -813,7 +813,7 @@ class UploadController extends Controller
 
                 if ((Yii::$app->request->post('ListsForm')['id'] !== '') || (Yii::$app->request->post('ListsForm')['url'] !== '')) {
                     // Listado ListView
-                    return $this->redirect(array('lists_crud/index',
+                    return $this->redirect(array('upload/lists/index',
                         'title' => 'Listado',
                         'return' => 'lists',
                         'model' => $model,
---
+++ views/upload/lists/index.php
@@ -0,0 +1,56 @@
+<?php
+// YOUR_APP/views/lists/index.php
+
+use yii\widgets\ListView;
+use yii\helpers\Html;
+?>
+<div class="header">
+    <h3><?= Html::encode($title); ?> Git</h3>
+</div>
+<div class="row">
+    <h4><pre>UPLOAD</pre></h4>
+    <h5><pre>Nombre                                   Publicación            Git            Acción</pre></h5>
+    <?= ListView::widget([
+        'options' => [
+            'tag' => 'div',
+        ],
+        'dataProvider' => $listDataProvider,
+        'itemView' => function ($model, $key, $index, $widget) {
+            $itemContent = $this->render('_list_item',['model' => $model]);
+
+            /* Display an Advertisement after the first list item */
+            if ($index == 0) {
+                $adContent = $this->render('_ad');
+                $itemContent .= $adContent;
+            }
+
+            return $itemContent;
+
+            /* Or if you just want to display the list item only: */
+            // return $this->render('_list_item',['model' => $model]);
+        },
+        'itemOptions' => [
+            'tag' => false,
+        ],
+        'summary' => '',
+
+        /* do not display {summary} */
+        'layout' => '{items}{pager}',
+
+        'pager' => [
+            'firstPageLabel' => 'First',
+            'lastPageLabel' => 'Last',
+            'maxButtonCount' => 4,
+            'options' => [
+                'class' => 'pagination col-xs-12'
+            ]
+        ],
+
+    ]);
+    ?>
+</div>
+<hr/>
+<p><a class="btn btn-lg btn-warning" href="index.php?r=crud%2F<?= Html::encode($return); ?>">Atrás</a></p>
+<div class="footer">
+</div>
+
