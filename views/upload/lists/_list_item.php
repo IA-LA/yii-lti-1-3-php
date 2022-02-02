50c8030be372d68ac9321473434511bd1435e781
FJ <fsanchez@lsi.uned.es>
2022-02-02 11:33:06 +0100

    Yii: Lists. Actualiza Vista LTI, Platfofm, Upload y Site.

--- views/upload/lists/_list_item.php
+++ views/upload/lists/_list_item.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 ?>
 
 <article class="list-item col-sm-12" data-key="<?= $model['id'] ?>">
-    <h5><?= Html::encode($model['id']); ?></h5>
+    <h4><?= Html::encode($model['id']); ?></h4>
     <h5>
         <?= $model['title'] ?> <?= $model['link'] ?>  <?= $model['link1'] ?> <img src="<?= $model['image'] ?>" alt="<?= $model['id'] ?>" width="30"> <?= $model['buttonC'] ?> <?= $model['buttonR'] ?> <?= $model['buttonU'] ?> <?= $model['buttonD'] ?>
     </h5>
