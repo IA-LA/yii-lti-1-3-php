399fe87f80fc6b38e0a80ee2751c760d7f9b905b
FJ <fsanchez@lsi.uned.es>
2022-02-16 14:50:42 +0100

    Yii: CRUD. Actualiza Controlador Listado en crud, Lti, Platfofm, Upload y Site.            Actualiza Modelo Listado crud, Lti, Platfofm, Upload y Site.            Actualiza Vista Listado en  crud, Lti, Platfofm, Upload y Site.

--- views/upload/lists/_list_item.php
+++ views/upload/lists/_list_item.php
@@ -33,7 +33,7 @@ use yii\bootstrap\Modal;
                     <pre>';
             print_r($model['data']);
             echo '</pre>';
-            $this->redirect(array('//upload/crud/read',['model' => $modelR, 'id' => '*']));
+            $this->render('//upload/crud/read',['model' => $modelR, 'id' => '*']);
             Modal::end();
         ?>
         <?= $model['buttonU'] ?> <?= $model['buttonD'] ?>
