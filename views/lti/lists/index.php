62b68faff6686938d088a75ac63adeb223328e56
FJ <fsanchez@lsi.uned.es>
2022-02-18 06:59:02 +0100

    Yii: CRUD. Actualiza Controlador Lists en Lti, Platfofm, Upload y Site.            Actualiza Modelo Lists en Lti, Platfofm, Upload y Site.            Actualiza Vista Lists en Lti, Platfofm, Upload y Site.

--- views/lti/lists/index.php
+++ views/lti/lists/index.php
@@ -21,11 +21,11 @@ Url::remember();
         ],
         'dataProvider' => $listDataProvider,
         'itemView' => function ($model, $key, $index, $widget) {
-            $itemContent = $this->render('lti/_list_item',['model' => $model]);
+            $itemContent = $this->render('lists/_list_item',['model' => $model]);
 
             /* Display an Advertisement after the first list item */
             if ($index == 0) {
-                $adContent = $this->render('lti/_ad');
+                $adContent = $this->render('lists/_ad');
                 $itemContent .= $adContent;
             }
 
