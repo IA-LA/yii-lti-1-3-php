d1313b285882e312ae2314d056a502719abca72f
FJ <fsanchez@lsi.uned.es>
2022-03-02 23:21:09 +0100

    Yii: CRUD.     Actualiza Controlador CRUD en Lti, Platfofm, Upload y Site.     Actualiza Modelo CRUD en Lti, Platfofm, Upload y Site.     Actualiza Vista CRUS en Lti, Platfofm, Upload y Site.

--- views/platform/crud/create.php
+++ views/platform/crud/create.php
@@ -51,9 +51,9 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'auth_login_url')->textInput(['value' => isset($auth_login_url)? $auth_login_url : ' ']) ?>
                 <?= $form->field($model, 'auth_token_url')->textInput(['value' => isset($auth_token_url)? $auth_token_url : ' ']) ?>
                 <?= $form->field($model, 'key_set_url')->textInput(['value' => isset($key_set_url)? $key_set_url : ' ']) ?>
-                <?= $form->field($model, 'kid')->hint(isset($kid)? $kid : ' ') ?>
+                <?= $form->field($model, 'kid')->hiddenInput(['value' => isset($kid)? $kid : ' '])->label(false) ?>
                 <?= $form->field($model, 'deployment')->textInput(['value' => isset($deployment)? $deployment : ' ']) ?>
-                <?= $form->field($model, 'auth_server')->hint(isset($auth_server)? $auth_server : ' ') ?>
+                <?= $form->field($model, 'auth_server')->hiddenInput(['value' => isset($auth_server)? $auth_server : ' '])->label(false) ?>
 
                 <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
                     'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
