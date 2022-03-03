5baf9cc98d7d8553c8e22aec7e830c559e812977
FJ <fsanchez@lsi.uned.es>
2022-03-03 13:02:42 +0100

    Yii: Upload.     Actualiza Controlador Upload.     Actualiza Modelo Upload.     Actualiza Vista Upload.

--- views/upload/upload.php
+++ views/upload/upload.php
@@ -354,11 +354,11 @@ Url::remember();
                             Espere por favor ...
                             Thank you.
                         </pre>';
-                        $form = ActiveForm::begin(['id' => 'modal-upload-form']);
+                        $form1 = ActiveForm::begin(['id' => 'modal-upload-form']);
 
                         echo '<!-- Modal de Upload -->';
-                        $form->field($model, 'zipFile')->fileInput();
-                        $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                        $form1->field($model, 'zipFile')->fileInput();
+                        $form1->field($model, 'verifyCode')->widget(Captcha::className(), [
                             'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
                         ]);
                         echo '<!-- UPLOAD Bad Request (#400) Unable to verify your data submission.   -->
