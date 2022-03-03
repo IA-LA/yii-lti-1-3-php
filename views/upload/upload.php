1b8e8ac73d9c183db80b82e905c3b0b8cfeb1c56
FJ <fsanchez@lsi.uned.es>
2022-03-03 23:12:45 +0100

    Yii: Upload.     Actualiza Controlador Upload.     Actualiza Modelo Upload.     Actualiza Vista Upload.

--- views/upload/upload.php
+++ views/upload/upload.php
@@ -347,7 +347,7 @@ Url::remember();
                         Modal::end();
                     ?>
 
-                    <?= Html::submitButton('Upload', ['class' => 'btn btn-primary', 'name' => 'modal-upload-button',  'data-toggle' => 'modal', 'data-target' => '#modal-u', 'onclick' => 'Yii::$app->session->setFlash("uploadingFormSubmitted")']) ?>
+                    <?= Html::Button('Upload', ['class' => 'btn btn-primary', 'name' => 'modal-upload-button',  'data-toggle' => 'modal', 'data-target' => '#modal-u']) ?>
 
                 </div>
             </div>
