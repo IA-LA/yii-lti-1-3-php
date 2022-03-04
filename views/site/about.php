985af95b5ca7fb29e376eaa8638f0ebf90da414a
FJ <fsanchez@lsi.uned.es>
2022-03-04 07:49:40 +0100

    Yii: Main y About.     Actualiza Menú.     Actualiza TODO & DONE.

--- views/site/about.php
+++ views/site/about.php
@@ -29,9 +29,10 @@ $this->params['breadcrumbs'][] = $this->title;
             <li><del><samp>DONE</samp></del> Registrar automáticamente ZIP + URL y Proyecto Git de un Upload en el lServidor LTI</li>
             <li><del><samp>DONE</samp></del> Platform Menu</li>
             <li><del><samp>DONE</samp></del> Update Form (Git, Lti y Platform)</li>
-            <li><del><samp>DONE</samp></del> <li>List con opciones CRUD</li></li>
+            <li><del><samp>DONE</samp></del> List con opciones CRUD</li>
+            <li><code>TODO</code> List de Upload con opciones CRUD</li>
 
-            <b>PLATFORMAS LMS</b>
+            <b>PLATAFORMAS LMS</b>
             <li><del><samp>DONE</samp></del> Proveer <a href="http://ailanto-dev.intecca.uned.es:9002/login.php?iss=10120210421153903000000a&login_hint=123456&target_link_uri=https://ailanto-dev.intecca.uned.es/lti/publicacion/10120210421153903000000a&lti_message_hint=123456" target="_blank">una Actividad <b>HTTP</b> no LTI</a> (<a href="https://ailanto-dev.intecca.uned.es/lti/publicacion/10120210421153903000000a" target="_blank">eContent</a>) en entornos LMS (<a href="http://ailanto-dev.intecca.uned.es:8080/courses/course-v1:edX+DemoX+Demo_Course/courseware/17dbb7a0acaa453e9811b67028973ca0/48f40f1eda24409384eb6a1415536c01/1?activate_block_id=block-v1%3AedX%2BDemoX%2BDemo_Course%2Btype%40vertical%2Bblock%407a2650b00f89411792c3ab18fa631aa5" target="_blank">EdX</a>, <a href="https://ailanto-dev.intecca.uned.es/eTrivial/course/view.php?id=52#section-0" target="_blank">Moodle</a>, etc)</li>
             <li><code>TODO</code> Proveer una Actividad <b>HTTPS</b> no LTI (eContent) en entornos LMS (<a href="http://ailanto-dev.intecca.uned.es:8080" target="_blank">EDX</a>, <a href="https://ailanto-dev.intecca.uned.es/eTrivial/" target="_blank">Moodle</a>, etc)</li>
             <li><del><samp>DONE</samp></del> Proveer un <a href="https://lti-ri.imsglobal.org/platforms/2630/resource_links/62041" target="_blank">Recurso LTI 1.3 (LtiResourceLinkRequest)</a> <a href="https://lti-ri.imsglobal.org/platforms/2630/resource_links/62041/rosters" target="_blank">vía OIDC</a> en plataformas <a href="https://lti-ri.imsglobal.org/platforms/2630" target="_blank">IMS Global</a> simuladas</li>
@@ -48,85 +49,142 @@ $this->params['breadcrumbs'][] = $this->title;
                 <h3>Upload Activity</h3>
 
                 <p>Permite subir y registrar contenidos complejos, en un único archivo .zip sin espacios en blanco, eñes o tildes en
-                    el nombre, el cual se utilizará como plantilla para publicarlo en una URL, registrarlo en el Servidor
+                    el nombre que se utilizará como plantilla para publicarlo en una URL, registrarlo en el Servidor
                     LTI y como base de un proyecto Git que permita trabajar en él de forma distribuida.</p>
 
-                <p><a class="btn btn-default" href="index.php?r=site%2Fupload">Upload &raquo;</a></p>
-                <p><a class="btn btn-default" href="index.php?r=site%2Fuploadregister">Upload & Register &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=upload%2Fupload">Upload &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=upload%2Fuploadregister">Upload & Register &raquo;</a></p>
 
                 <hr/>
                 <h3>Publish Project</h3>
 
-                <p>Permite publicar y actualizar contenidos complejos, con un id sin espacios en blanco, eñes o tildes en
-                    el nombre, el cual se utilizará como plantilla para publicarlo en una URL, registrarlo en el Servidor
-                    LTI y como base de un proyecto Git que permita trabajar en él de forma distribuida.</p>
+                <p>Permite publicar y registrar el proyecto Git de contenidos complejos, con un id sin espacios en blanco, eñes o tildes en
+                    el nombre, para trabajar en él de forma distribuida.</p>
 
-                <p><a class="btn btn-default" href="index.php?r=crud%2Fpublish">Publish &raquo;</a></p>
-                <p><a class="btn btn-default" href="index.php?r=crud%2Fpublishregister">Publish & Register &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=upload%2Fpublish">Publish &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=upload%2Fpublishregister">Publish & Register &raquo;</a></p>
 
             </div>
-            <div class="col-lg-4">
-                <h2>Git Menu</h2>
-                <hr/>
-                <h3>Delete</h3>
 
-                <p>Formulario de borrado de Proyectos (Git) dadas de alta en el servior.</p>
-
-                <p><a class="btn btn-default" href="index.php?r=crud%2Fdelete">Borrado &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h3>Lists</h3>
-
-                <p>Formulario de listados de Proyectos (Git) dadas de alta en el servior.</p>
-
-                <p><a class="btn btn-default" href="index.php?r=crud%2Flists">Listados &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h3>Query</h3>
-
-                <p>Formulario de consulta de Proyectos (Git) dadas de alta en el servior.</p>
-
-                <p><a class="btn btn-default" href="index.php?r=crud%2Fquery">Consulta &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h3>Register</h3>
-
-                <p>Formulario que permite dar de alta un Proyecto (Git) para su gestión.</p>
-
-                <p><a class="btn btn-default" href="index.php?r=crud%2Fregister">Registro &raquo;</a></p>
-            </div>
             <div class="col-lg-4">
                 <h2>LTI Menu</h2>
                 <hr/>
-                <h3>Delete</h3>
-
-                <p>Formulario de borrado de Actividades (Tools) dadas de alta en el servior.</p>
-
-                <p><a class="btn btn-default" href="index.php?r=site%2Fdelete">Borrado &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h3>Lists</h3>
 
                 <p>Formulario de listados de Actividades (Tools) dadas de alta en el servior.</p>
 
-                <p><a class="btn btn-default" href="index.php?r=site%2Flists">Listados &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=lti%2Flists">Listados &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h3>Query</h3>
-
-                <p>Formulario de consulta de Actividades (Tools) dadas de alta en el servior.</p>
-
-                <p><a class="btn btn-default" href="index.php?r=site%2Fquery">Consulta &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h3>Register</h3>
+                <h3>Create</h3>
 
                 <p>Formulario que permite dar de alta una Actividad (Tool) para su difusión.</p>
 
-                <p><a class="btn btn-default" href="index.php?r=site%2Fregister">Registro &raquo;</a></p>
+                <p><a class="btn btn-default" href="index.php?r=lti%2Fcreate">Registro &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Read</h3>
+
+                <p>Formulario de consulta de Actividades (Tools) dadas de alta en el servior.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=lti%2Fread">Consulta &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Update</h3>
+
+                <p>Formulario que permite actualizar Actividades (Tools) dadas de alta en el servior.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=lti%2Fupdate">Registro &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3>Delete</h3>
+
+                <p>Formulario de borrado de Actividades (Tools) dadas de alta en el servior.</p>
+
+                <p><a class="btn btn-default" href="index.php?r=lti%2Fdelete">Borrado &raquo;</a></p>
             </div>
         </div>
 
-    </div>
+        <div class="col-lg-4">
+            <h2>Platform Menu</h2>
+            <hr/>
+        </div>
+        <div class="col-lg-4">
+            <h3>Lists</h3>
 
+            <p>Formulario de listados de Plataformas (LMSs) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=platform%2Flists">Listados &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Create</h3>
+
+            <p>Formulario que permite dar de alta una Plataforma (LMS) para su difusión.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=platform%2Fcreate">Registro &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Read</h3>
+
+            <p>Formulario de consulta de Plataformas (LMSs) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=platform%2Fread">Consulta &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Update</h3>
+
+            <p>Formulario que permite actualizar Plataformas (LMSs) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=platform%2Fupdate">Registro &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Delete</h3>
+
+            <p>Formulario de borrado de Plataformas (LMSs) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=platform%2Fdelete">Borrado &raquo;</a></p>
+        </div>
+
+        <div class="col-lg-4">
+            <h2>Upload Menu</h2>
+            <hr/>
+        </div>
+        <div class="col-lg-4">
+            <h3>Lists</h3>
+
+            <p>Formulario de listados de Proyectos (Git) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=upload%2Flists">Listados &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Create</h3>
+
+            <p>Formulario que permite dar de alta un Proyecto (Git) para su gestión.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=crud%2Fcreate">Registro &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Read</h3>
+
+            <p>Formulario de consulta de Proyectos (Git) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=upload%2Fread">Consulta &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Update</h3>
+
+            <p>Formulario que permite actualizar Proyectos (Git) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=upload%2Fupdate">Borrado &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h3>Delete</h3>
+
+            <p>Formulario de borrado de Proyectos (Git) dadas de alta en el servior.</p>
+
+            <p><a class="btn btn-default" href="index.php?r=upload%2Fdelete">Borrado &raquo;</a></p>
+        </div>
+    </div>
 </div>
