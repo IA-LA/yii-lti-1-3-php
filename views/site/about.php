29cd78c983fd0407164605b16d214e1da48401c3
FJ <fsanchez@lsi.uned.es>
2022-03-04 10:44:05 +0100

    Yii: Main y About.     Actualiza Menú.     Actualiza TODO & DONE.

--- views/site/about.php
+++ views/site/about.php
@@ -46,7 +46,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-lg-4">
                 <h2>Main Menu</h2>
                 <hr/>
-                <h3>Upload LTI Activity (Upload Menu)</h3>
+                <h3>Upload Activity (Upload Menu)</h3>
 
                 <p>Permite subir y registrar contenidos complejos (eContent), en un único archivo .zip sin espacios en blanco, eñes o tildes en
                     el nombre que se utilizará como plantilla para publicarlo en una URL, registrarlo en el Servidor
