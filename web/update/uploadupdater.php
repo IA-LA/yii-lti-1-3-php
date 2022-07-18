6d70c747afa5674f4bccfb6368d6cc0855b2b9c3
FJ <fsanchez@lsi.uned.es>
2022-07-18 14:04:44 +0200

    Yii: Updater. Actualiza updater de Upload.

--- web/update/uploadupdater.php
+++ web/update/uploadupdater.php
@@ -75,7 +75,7 @@ $url=$_SERVER;
                 exec(escapeshellcmd('mkdir uploads/difusion/' . $namedir), $output, $retval);
             ?>
 
-                <p class="alert alert-success">Archivo ´<b><i><?= $retval ?></i></b>´ recibido correctamente <?= (file_exists($_REQUEST['namedir']) ? 'y existe' : 'pero no existe') ?></p>
+                <p class="alert alert-success">Archivo ´<b><i><?= print_r($output) ?></i></b>´ recibido correctamente <?= (file_exists($_REQUEST['namedir']) ? 'y existe' : 'pero no existe') ?></p>
 
             <?php
                 // MKDIR sin errores
