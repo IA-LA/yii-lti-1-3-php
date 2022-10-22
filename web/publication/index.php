1183bb7bd62bb2c5bc1db174c3d429cfc26002eb
FJ <fsanchez@lsi.uned.es>
2022-10-22 09:26:17 +0200

    Yii: Publication. Redir inyectando file_get_content() y substr_replace().

--- web/publication/index.php
+++ web/publication/index.php
@@ -198,7 +198,7 @@ if (isset($_REQUEST['id']) || (isset($_REQUEST['id']) && isset($_REQUEST['iss'])
                 //echo file_get_contents('https://ailanto-dev.intecca.uned.es/lti/publicacion/10220210903095251000000a/xml/');
                 //echo file_get_contents('https://ailanto-dev.intecca.uned.es/lti/publicacion/10220210903095251000000a/index.html');
                 $html = file_get_contents('https://ailanto-dev.intecca.uned.es/lti/publicacion/10220210903095251000000a/index.html');
-                $html= substr_replace($html, '<base href="https://ailanto-dev.intecca.uned.es/lti/publicacion/10220210903095251000000a/">',136, null);
+                $html= substr_replace($html, '        <base href="https://ailanto-dev.intecca.uned.es/lti/publicacion/10220210903095251000000a/"/>',136, null);
                 echo $html;
                 die;
 
