36e859ad9247ed166a77787d1a7b9db3b136ccbb
FJ <fsanchez@lsi.uned.es>
2023-01-24 19:59:11 +0100

    Yii: index. Upload. Git pull Actividad LTI

--- web/upload/index.php
+++ web/upload/index.php
@@ -499,7 +499,7 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 $output=null;
                 $retval=null;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ push origin master'), $output, $retval);
-                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ push origin master --force'), $output, $retval);
+                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ pull origin master'), $output, $retval);
                 echo "10.Returned with status $retval and output:\n";
                 //echo "<p><pre>10.a. git -C ../uploads/publicacion/$namedir/ push origin master<br/>";
                 //echo "10.PassThru" . passthru('git -C ../uploads/publicacion/' . $namedir . '/ push origin master 2>&1') . "<br/>";
