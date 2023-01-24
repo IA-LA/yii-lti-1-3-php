1224ee089eea77ad9fe81431d5a8aa6c36485c9a
FJ <fsanchez@lsi.uned.es>
2023-01-24 13:29:11 +0100

    Yii: index. Upload. URL crear Actividad LTI

--- web/upload/index.php
+++ web/upload/index.php
@@ -382,13 +382,13 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
             }
         endif;
 
+        // TODO-NE unzip fichero .ZIP en Carpeta Difusion
+
         ///
         // Difundir la Actividad
         // Descomprime archivo Zip 'unzip -o -X ../uploads/difusion/'
         //  ID=$namedir
         ///////////////////////////////////////////////////////////////////////////////////
-
-        // TODO-NE unzip .ZIP en  Carpeta Difusion
 
         // COPIA el archivo .zip en la carpeta de difusion
         /////////////////////////////////////////////////
@@ -415,10 +415,10 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 exec(escapeshellcmd('unzip -o -X ../uploads/difusion/' . $namedir . '/' . $_REQUEST['file'] . ' -d ../uploads/publicacion/' . $namedir), $output, $retval);
                 exec(escapeshellcmd('chmod 774 -R ../uploads/publicacion/' . $namedir), $output, $retval);
                 //exec(escapeshellcmd('unzip ../uploads/cindetechtmlv1_5a5db903d3bd0d7623bc10c0.zip -d ../uploads/publicacion/' . $namedir), $output, $retval);
-                //echo "6.Returned with status $retval and output:\n";
+                echo "6.Returned with status $retval and output:\n";
                 //echo "<i> " . count($output) . " archivos descomprimidos. Status y resultado " . ($retval === 0 ? 'correctos' : 'erróneos') . ":\n</i>";
                 //echo "<p><pre> 6.a. Unzip PassThru " . passthru('unzip -o -X ../uploads/' . $file . ' -d ../uploads/publicacion/' . $namedir . ' 2>&1') . "<br/>";
-                //print_r($output);
+                print_r($output);
                 //echo "</pre></p>";
                 //$output = shell_exec(escapeshellcmd('unzip -o -X ../uploads/' . $file . ' -d ../uploads/publicacion/' . $namedir));
                 //$output = shell_exec(escapeshellcmd('echo "Hola Mundo Linux" >> ../uploads/publicacion/' . $namedir . '/HolaMundo.txt'));
@@ -436,14 +436,14 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 $retval=null;
                 exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ reset '), $output, $retval);
                 exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ add . '), $output, $retval);
-                //echo "7.Returned with status $retval and output:\n";
+                echo "7.Returned with status $retval and output:\n";
                 //echo "<p><pre>7.a. PassThru " . passthru('git -C ../uploads/publicacion/' . $namedir . '/ add . 2>&1') . "<br/>";
                 //print_r($output);
                 //echo "</pre></p>";
                 //$output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ add .'));
                 //echo "<pre>7.b. $output</pre>";
                 //$output = shell_exec(escapeshellcmd('sleep 0.5s'));
-                //echo "<pre>7.c. $output</pre>";
+                echo "<pre>7.c. $output</pre>";
 
                 // Commit Config
                 // outputs the username that owns the running php/httpd process
@@ -454,19 +454,19 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.email "you@example.com"'), $output, $retval);
                 //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.email "' . Yii::$app->user->identity->username . '@lti.server"'), $output, $retval);
                 exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.email "' . 'gcono' . '@lti.server"'), $output, $retval);
-                //echo "8.Returned with status $retval and output:\n";
+                echo "8.Returned with status $retval and output:\n";
                 //echo "<p><pre>8.a.<br/>";
                 //echo "8.a.PassThru " . passthru('git -C ../uploads/publicacion/' . $namedir . ' config user.email "you@example.com" 2>&1') . "<br/>";
-                //print_r($output);
+                print_r($output);
                 //echo "</pre></p>";
                 // outputs the username that owns the running php/httpd process
                 // (on a system with the "git add" executable in the path)
-                $output=null;
-                $retval=null;
+                //$output=null;
+                //$retval=null;
                 //exec(escapeshellcmd('git config --global user.name "Your Name"'), $output, $retval);
                 //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.name "Your Name"'), $output, $retval);
                 //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.name "' . Yii::$app->user->identity->id . '"'), $output, $retval);
-                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.name "' . '000' . '"'), $output, $retval);
+                //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . ' config --local user.name "' . '000' . '"'), $output, $retval);
 
                 //echo "8.Returned with status $retval and output:\n";
                 //echo "<p><pre>8.b.<br/>";
@@ -485,10 +485,10 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 $retval=null;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ commit -m "Init Commit Server LTI"'), $output, $retval);
                 exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Difusion Commit Server LTI"'), $output, $retval);
-                //echo "9.Returned with status $retval and output:\n";
+                echo "9.Returned with status $retval and output:\n";
                 //echo "<p><pre>9.a. git -C ../uploads/publicacion/$namedir/ commit -m 'Initial Commit Server LTI' <br/>";
                 //echo "9.PassThru" . passthru('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Initial Commit Server LTI" 2>&1') . "<br/>";
-                //print_r($output);
+                print_r($output);
                 //echo "</pre></p>";
                 //$output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Initial Commit Server LTI" 2>&1'));
                 //echo "<pre>9.b. $output</pre>";
@@ -500,10 +500,10 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 $retval=null;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ push origin master'), $output, $retval);
                 exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ push origin master'), $output, $retval);
-                //echo "10.Returned with status $retval and output:\n";
+                echo "10.Returned with status $retval and output:\n";
                 //echo "<p><pre>10.a. git -C ../uploads/publicacion/$namedir/ push origin master<br/>";
                 //echo "10.PassThru" . passthru('git -C ../uploads/publicacion/' . $namedir . '/ push origin master 2>&1') . "<br/>";
-                //print_r($output);
+                print_r($output);
                 //echo "</pre></p>";
                 //$output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ push origin master 2>&1'));
                 //echo "<pre>10.b. $output</pre>";
