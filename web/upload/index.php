a64a091682c7a4999efaf48ab3d046db5fb61a9e
FJ <fsanchez@lsi.uned.es>
2023-01-24 20:33:31 +0100

    Yii: index. Upload. Actividad LTI

--- web/upload/index.php
+++ web/upload/index.php
@@ -436,11 +436,11 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 // (on a system with the "git add" executable in the path)
                 $output=null;
                 $retval=null;
-                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ reset '), $output, $retval);
-                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ add . '), $output, $retval);
-                echo "7.Returned with status $retval and output:\n";
+                //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ reset '), $output, $retval);
+                //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ add . '), $output, $retval);
+                //echo "7.Returned with status $retval and output:\n";
                 //echo "<p><pre>7.a. PassThru " . passthru('git -C ../uploads/publicacion/' . $namedir . '/ add . 2>&1') . "<br/>";
-                print_r($output);
+                //print_r($output);
                 //echo "</pre></p>";
                 //$output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ add .'));
                 //echo "<pre>7.b. $output</pre>";
@@ -486,11 +486,11 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 $output=null;
                 $retval=null;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ commit -m "Init Commit Server LTI"'), $output, $retval);
-                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Difusion Commit Server LTI"'), $output, $retval);
-                echo "9.Returned with status $retval and output:\n";
+                //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Difusion Commit Server LTI"'), $output, $retval);
+                //echo "9.Returned with status $retval and output:\n";
                 //echo "<p><pre>9.a. git -C ../uploads/publicacion/$namedir/ commit -m 'Initial Commit Server LTI' <br/>";
                 //echo "9.PassThru" . passthru('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Initial Commit Server LTI" 2>&1') . "<br/>";
-                print_r($output);
+                //print_r($output);
                 //echo "</pre></p>";
                 //$output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ commit -m "Initial Commit Server LTI" 2>&1'));
                 //echo "<pre>9.b. $output</pre>";
@@ -501,13 +501,13 @@ if ((isset($_REQUEST['file']) && isset($_REQUEST['namedir'])) && ((($_REQUEST['f
                 $output=null;
                 $retval=null;
                 //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ push origin master'), $output, $retval);
-                exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ push origin master --force'), $output, $retval);
-                echo "10.Returned with status $retval and output:\n";
+                //exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ push origin master --force'), $output, $retval);
+                //echo "10.Returned with status $retval and output:\n";
                 //echo "<p><pre>10.a. git -C ../uploads/publicacion/$namedir/ push origin master<br/>";
                 //echo "10.PassThru" . passthru('git -C ../uploads/publicacion/' . $namedir . '/ push origin master 2>&1') . "<br/>";
-                print_r($output);
+                //print_r($output);
                 //echo "</pre></p>";
-                $output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ pullorigin master 2>&1'));
+                $output = shell_exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ pull origin master 2>&1'));
                 //echo "<pre>10.b. $output</pre>";
 
                 // Git, UNZIP y Publicacion sin errores
