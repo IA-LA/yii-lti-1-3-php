e3857a46476974fcb8c7e99c781c05bfb1a5ab60
FJ <fsanchez@lsi.uned.es>
2023-07-18 16:16:51 +0200

    Yii: Web Services. Update.

--- web/update/index.php
+++ web/update/index.php
@@ -352,6 +352,21 @@ $url=$_SERVER;
                             //$output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ commit -m "Update Commit Server LTI" 2>&1'));
                             //echo "<pre>9.b. $output</pre>";
 
+                            // Push clonado
+                            // outputs the username that owns the running php/httpd process
+                            // (on a system with the "git add" executable in the path)
+                            $output=null;
+                            $retval=null;
+                            //exec(escapeshellcmd('git -C ' . $carpetaGit . '/uploads/publicacion/' . $namedir . '/ push origin master'), $output, $retval);
+                            exec(escapeshellcmd('git -C ../uploads/publicacion/' . $namedir . '/ push origin master'), $output, $retval);
+                            echo "10.Returned with status $retval and output:\n";
+                            echo "<p><pre>10.a. git -C uploads/publicacion/$namedir/ push origin master<br/>";
+                            echo "10.PassThru" . passthru('git -C uploads/publicacion/' . $namedir . '/ push origin master 2>&1') . "<br/>";
+                            print_r($output);
+                            //echo "</pre></p>";
+                            //$output = shell_exec(escapeshellcmd('git -C uploads/publicacion/' . $namedir . '/ push origin master 2>&1'));
+                            //echo "<pre>10.b. $output</pre>";
+
                             // Unzip Actividad .zip
                             // outputs the username that owns the running php/httpd process
                             // (on a system with the "unzip" executable in the path)
