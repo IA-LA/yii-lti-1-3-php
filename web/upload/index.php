36a6ac717da9a775c0f608e9731d5535405df39b
FJ <fsanchez@lsi.uned.es>
2023-07-24 09:33:17 +0200

    Yii: Web Services. Update. Control excepciones.                    Upload. Control excepciones.                    Publicaction. Excepciones.

--- web/upload/index.php
+++ web/upload/index.php
@@ -13,7 +13,7 @@ set_error_handler(
     function ($err_severity, $err_msg, $err_file, $err_line, array $err_context)
     {
         throw new ErrorException( $err_msg, 0, $err_severity, $err_file, $err_line );
-    }, E_WARNING);
+    }, E_ALL);
 
 try{
 // TODO MULTIPROCESO/MULTITAREA
